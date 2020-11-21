74b37e8cda8e2f3d4e60ea4da729fecafedd1981
malkeeszon <rwitczak89@gmail.com>
2020-11-21 10:38:05 +0100

    prepares rest.test.js to tests

--- server/express/__tests__/rest.test.js
+++ server/express/__tests__/rest.test.js
@@ -1,8 +1,5 @@
-describe("testing REST routes", () => {
-
+describe('testing REST routes', () => {
   test('Coolest test', () => {
-    expect(1).toBe(1)
+    expect(1).toBe(1);
   });
-
-  
-};
+});
