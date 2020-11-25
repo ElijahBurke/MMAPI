0599b7329b6ee3a95b2eff16cd48b942ee27a89d
Mandy Nijssen <nijssenmandy89@gmail.com>
2020-11-25 11:21:59 +0100

    test: delete front-end hero test

--- mmapi-client/src/__tests__/hero.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Banner from '../components/Banner/banner'
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-  const banner = renderer.create(<Banner />).toJSON();
-  expect(banner).toMatchSnapshot();
-})
